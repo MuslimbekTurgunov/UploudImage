3f6dff431a9c6871a4a34bb2706cad41e6cd2a30
MuslimbekTurgunov <turgunovmuslimbek>
2022-03-26 10:27:51 +0500

    Initial Commit

---
+++ Vazifa_63.UploudImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vazifa_63.UploudImage
+//
+//  Created by Macbook on 26/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
